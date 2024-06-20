73f48729736138c9ee75a77009a88defb69d5dca
Nayli <22082010090@student.upnjatim.ac.id>
2024-06-20 09:24:38 +0700

    ganti template atas

--- pembayaran.php
+++ pembayaran.php
@@ -12,7 +12,7 @@ require_once('auth.php');
 
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 
-  <title>Tale SEO Agency - FAQ Page</title>
+  <title>SIWARSA</title>
 
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
